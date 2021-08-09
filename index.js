9e0d23ab13b295a4ea3bf7fffe103313dd9552bb
LAXMIKANT DANGE <ldange@linecorp.com>
2021-08-10 01:38:34 +0900

    added cors

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
 const express = require('express')
+var cors = require('cors')
+
 const app = express()
 const port = process.env.PORT || 3000
+
+app.use(cors())
 
 app.use(express.static('./'))
 
